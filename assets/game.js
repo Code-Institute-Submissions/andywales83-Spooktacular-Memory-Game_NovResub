85f31e6e499755f3a0c3825fa9bf98258883ce3d
Andrew Llewellyn <andrew.llewellyn83@gmail.com>
2021-09-15 19:09:27 +0000

    add logic for card flip and add data attribute for card matching function

--- assets/game.js
+++ assets/game.js
@@ -1,9 +1,26 @@
 // List memory card elements
 const cards = document.querySelectorAll('.game-card');
 
+// logic for first and second card flip
+let hasFlippedCard = false;
+let firstCard, secondCard;
+
 // declare flipCard function
 function flipCard() {
-    this.classList.toggle('flip');
+    this.classList.add('flip');
+
+    // condition for fliping card on click
+    if (!hasFlippedCard) {
+        // first click
+        hasFlippedCard = true;
+        firstCard = this;
+    } else {
+        // second click
+        hasFlippedCard = false;
+        secondCard = this;
+
+        // do cards match
+    }
 }
 
 // add click event listener
